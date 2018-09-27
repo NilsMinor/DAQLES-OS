46442a3d6ce600bdb22429f1a96e7210a046e95d
Nils Minor <nilsminor@web.de>
2018-09-27 10:23:20 +0200

    copy uEnv.txt from config to build and then to SD card

--- copy_files_to_SD_card.sh
+++ copy_files_to_SD_card.sh
@@ -3,21 +3,27 @@
 # tar old files to archiv folder with timestamp
 # remove old files
 
-BUILD_DIR=/opt/build/
+BUILD_DIR=/opt/build
+CONFIG_DIR=/opt/config
 SD_DIR="/media/psf/NO NAME/"
+
 echo "copy files to SD Card"
 
+# Copy uEnv config to build folder
+cp $CONFIG_DIR/uEnv.txt $BUILD_DIR/
+
 # Go to build folder and copy files to SD Card
-pushd $BUILD_DIR
+pushd $BUILD_DIR/
 ls -la
 if [ -d "$SD_DIR" ]; then
 #	cp -v uImage "$SD_DIR" 
 	cp -v *.BIN "$SD_DIR"
-
+	cp -v uEnv.txt "SD_DIR"
+	sync
 # Archive files
 pushd ..
-tar -cvf "archiv/build-$(date).tar" build/ 
-rm -r build/*
+	tar -cvf "archiv/build-$(date).tar" build/ 
+	rm -r build/*
 popd
 else
 	echo "SD Card not found at path $SD_DIR"
