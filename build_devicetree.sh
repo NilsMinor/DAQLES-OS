2b190aa8494f30741093d0eaf4512c8cc6787b7d
Nils Minor <nilsminor@web.de>
2018-09-28 09:08:57 +0200

    give build_devicetree function to generate dts from hdf

--- build_devicetree.sh
+++ build_devicetree.sh
@@ -2,12 +2,32 @@
 # Tool to build/rebuild devicetree
 
 INSTRUCTION="$1"
+BUILD_DIR=build_devicetree
+TARGET_DIR=/opt/build
+DTX_DIR=/opt/device-tree-xlnx
 
 # find name of file
 filename=$(basename -- "$1")
 filename="${filename%.*}"
 
-if [[ $1 == *.dtb ]] 
+if [[ $1 == *.hdf ]] 
+then
+pushd $TARGET_DIR
+
+	echo "build create_dt.tcl"
+	mkdir -p $BUILD_DIR
+	cp $1 $BUILD_DIR
+	echo "hsi open_hw_design `basename $1`" > $BUILD_DIR/create_dt.tcl	
+	echo "hsi set_repo_path $DTX_DIR/" > $BUILD_DIR/create_dt.tcl  
+ 	echo "hsi create_sw_design device-tree -os device_tree -proc ps7_cortexa9_0" > $BUILD_DIR/create_dt.tcl  
+	echo "hsi generate_target -dir `basename $1`"> $BUILD_DIR/create_dt.tcl    
+
+	echo "build devicetree"	
+	cd $BUILD_DIR
+        xsdk -batch -source create_dt.tcl
+popd
+
+elif [[$1 = *.dtb]] 
 then
 	echo "convert dtb > dts"	
 	dtc -I dtb -O dts -o $1 "$filename.dts" 
